2debcb4e504ee4cf7a9ce663768f5fc39e7bcdba
Christopher Vera Grajales <juan.osorio1@profuturo.com.mx>
2024-08-13 09:17:54 -0600

    proyecto usando records

---
+++ target/generated-sources/annotations/net/javaguides/springboot/mapper/AutoUserMapperImpl.java
@@ -0,0 +1,45 @@
+package net.javaguides.springboot.mapper;
+
+import javax.annotation.processing.Generated;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-12T12:25:57-0600",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240215-1558, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+public class AutoUserMapperImpl implements AutoUserMapper {
+
+    @Override
+    public UserDto mapToUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setEmail( user.getEmail() );
+        userDto.setFirstName( user.getFirstName() );
+        userDto.setId( user.getId() );
+        userDto.setLastName( user.getLastName() );
+
+        return userDto;
+    }
+
+    @Override
+    public User mapToUser(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setEmail( userDto.getEmail() );
+        user.setFirstName( userDto.getFirstName() );
+        user.setId( userDto.getId() );
+        user.setLastName( userDto.getLastName() );
+
+        return user;
+    }
+}
